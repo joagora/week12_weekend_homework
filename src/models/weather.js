556920be9f8bf99a91fd8037a6e19e1a7c9d58fd
Joanna Gora <joanna.gora@protonmail.com>
2018-09-14 15:06:06 +0100

    initial commit - initial setup

---
+++ src/models/weather.js
@@ -0,0 +1,9 @@
+const Weather = function() {
+  this.data = data;
+}
+
+Weather.prototype.getData = function() {
+
+}
+
+module.exports = Weather;
---
+++ src/views/search_view.js
@@ -0,0 +1,5 @@
+const SearchView = function(searchContainer) {
+  this.searchContainer = searchContainer;
+}
+
+module.exports = SearchView;
